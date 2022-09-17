1c00e5ec7e39b5e66dbc196a90c36a9490eb994e
webfaisalbd <web.faisal.bd@gmail.com>
2022-09-18 02:42:50 +0600

    email send and then get message

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzWf0lPitZFX9V1mhMxX-YS23Y6xr1Y8nzkhIU5AKzdnqZ03rzVCWXrsaENhwIaZYI/exec';
 // scriptURL er  link ta google sheet theke nite hobe
+
 const form = document.forms['submit-to-google-sheet']
 //  html er form tag er name er moddhe, 'submit-to-google-sheet' eita hubohu copy kore bosate hobe
+
+const newsletterMsg = document.getElementById("newsletter-msg");
+
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    .then(response => {
+        newsletterMsg.innerHTML = "Thank you for subscribing.";
+    })
     .catch(error => console.error('Error!', error.message))
 })
